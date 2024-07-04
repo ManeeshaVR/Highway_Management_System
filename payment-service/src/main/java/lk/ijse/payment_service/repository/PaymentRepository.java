374bc9d8ccaef8c2bcf1da2c9081a0af3524e011
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2024-07-04 22:16:16 +0530

    add payment crud

---
+++ payment-service/src/main/java/lk/ijse/payment_service/repository/PaymentRepository.java
@@ -0,0 +1,7 @@
+package lk.ijse.payment_service.repository;
+
+import lk.ijse.payment_service.entity.PaymentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<PaymentEntity, String> {
+}
